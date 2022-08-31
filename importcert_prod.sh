554d6807038f8993b64cb0fc5a79e6daaafc29c1
dasbatta <54562111+dasbatta@users.noreply.github.com>
2022-08-31 09:26:33 -0700

    Prod CA Import
    Prod CA Import

---
+++ importcert_prod.sh
@@ -0,0 +1,9 @@
+cd /tmp
+wget https://harbor-amco-qro-01.telmex.com/api/v2.0/systeminfo/getcert --no-check-certificate && \
+mv getcert rootint.crt && \
+openssl x509 -in rootint.crt -out rootint.pem && \
+cp rootint.pem /etc/ssl/certs/ && \
+cp rootint.crt /usr/local/share/ca-certificates/ && \
+sudo update-ca-certificates && \
+systemctl restart kubelet.service && \
+systemctl restart containerd.service
